12a70b370517f3d49970216bf323794ec0c946a3
Abviol <gogamerokiller@gmail.com>
2023-09-27 12:04:48 +0200

    initial commit
    The project was done before creating the repository, so this commit contains all the files needed.
    
    Here's the following:
    - index.html - the landing itself;
    - folder with assets which include CSS, imgs, and JS folders;
    - CSS folder contains styles.css file with all styles for the page, including CSS variables, reusable classes and media queries;
    - imgs folder contains all images used in Figma design;
    - js folder contains the main.js file, which is used for adding and removing some classes, e.g. .fixed for the header when it's scrolled by more than 50px;

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+﻿const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+const header = document.getElementById('header');
+
+menuToggle.addEventListener('click', () => {
+   menuToggle.classList.toggle('close');
+   navMenu.classList.toggle('show');
+})
+
+window.addEventListener('scroll', () => {
+   console.log(window.scrollY)
+   if (window.scrollY >= 100) {
+      header.classList.add('fix');
+   } else {
+      header.classList.remove('fix');
+   }
+})
+
+/* scroll to top button */
+const scrollTop = document.getElementById('scroll-to-top');
+window.addEventListener('scroll', () => {
+   console.log(window.scrollY)
+   if (window.scrollY >= 500) {
+      scrollTop.classList.add('show-button');
+   } else {
+      scrollTop.classList.remove('show-button');
+   }
+})
